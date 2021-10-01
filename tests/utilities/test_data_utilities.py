9f133ab6f7b7849ee4a62fdc6bdd757671051a13
Mark Needham <mark.needham@finbourne.com>
2021-10-01 12:41:47 +0100

    try a new scope

--- tests/utilities/test_data_utilities.py
+++ tests/utilities/test_data_utilities.py
@@ -10,11 +10,9 @@ from lusidumbrella import ApiClientFactory
 
 from tests.utilities import CredentialsSource
 
-import datetime
-
 
 class TestDataUtilities:
-    tutorials_scope = f"Testdemo_{str(datetime.datetime.now())}"
+    tutorials_scope = f"Testdemo_{datetime.now().strftime('%Y-%m-%d_%H_%M_%S')}"
     market_data_scope = "FinbourneMarketData"
 
     lusid_cash_identifier = "Instrument/default/Currency"
