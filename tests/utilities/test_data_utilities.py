36b871104632a5d8857fb2c4c9e2f4a70579a8c7
Mark Needham <mark.needham@finbourne.com>
2021-10-01 09:41:34 +0100

    unique scope

--- tests/utilities/test_data_utilities.py
+++ tests/utilities/test_data_utilities.py
@@ -12,7 +12,7 @@ from tests.utilities import CredentialsSource
 
 
 class TestDataUtilities:
-    tutorials_scope = "Testdemo"
+    tutorials_scope = f"Testdemo_{uuid.uuid4()}"
     market_data_scope = "FinbourneMarketData"
 
     lusid_cash_identifier = "Instrument/default/Currency"
